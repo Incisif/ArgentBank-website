09171edd90add37b78e92854ff299e25620c200a
incisif <loeildejack@gmail.com>
2023-08-04 02:56:45 +0200

    Added a redirect to the User page in the SignIn component after successful login, ensuring users are seamlessly navigated to their account dashboard.

--- argent-bank/src/pages/SignIn/index.jsx
+++ argent-bank/src/pages/SignIn/index.jsx
@@ -1,19 +1,29 @@
-
 import styled from "styled-components";
 import AuthForm from "../../components/AuthForm";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const SignInContainer = styled.div`
   background-color: #12002b;
   flex-grow: 1;
 `;
 
-
-
 function SignIn() {
+  const user = useSelector((state) => state.user.user);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      navigate("/user");
+    }
+  }, [user, navigate]);
+
   return (
     <SignInContainer>
       <AuthForm />
     </SignInContainer>
   );
 }
+
 export default SignIn;
